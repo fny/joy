e76198946d159075af85e10486a2620dcc26967f
Faraz Yashar <faraz.yashar@gmail.com>
2026-06-14 19:46:38 +0000

    remove dormant joy-daemon/joy-agent/joy-cli scaffold (unused redesign packages, nothing imports them)

--- packages/joy-cli/bin/joy.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { register } from 'tsx/esm/api';
-register();
-import('../src/index.ts').then((m) => m.main?.(process.argv.slice(2))).catch((e) => {
-    console.error(e);
-    process.exit(1);
-});
